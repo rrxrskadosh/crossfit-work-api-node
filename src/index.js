18ff47907a400f0f4346d23f38bf40323cf687bd
Roger Reyes <kadoshrr2019@gmail.com>
2023-07-28 21:57:35 -0500

    Implementation Names resources plural (Work Out Router)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import express from "express";
-import v1Router from "./v1/routes/index.js";
+import v1WorkoutRouter from "./v1/routes/workOutRouter.js";
 
 const app = express();
 const PORT = process.env.PORT || 3004;
 
-app.use('/api/v1', v1Router);
+app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
